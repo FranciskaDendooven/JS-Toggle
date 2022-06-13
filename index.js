681cc246002bf823c62b5980725eb138fb463d63
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-13 16:07:58 +0200

    Find img bug and finish assignment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-/* const phaseOne = document.querySelector("#phaseOne");
-const phaseTwo = document.querySelector("#phaseTwo");
-const phaseThree = document.querySelector("#phaseThree");
-const phaseFour = document.querySelector("#phaseFour"); */
 const button0 = document.querySelector(".button0");
 const button1 = document.querySelector(".button1");
 const button2 = document.querySelector(".button2");
 const button3 = document.querySelector(".button3");
 const inputName = document.querySelector("#name");
 const displayName = document.querySelector("#displayName");
+const yesNo = document.querySelector("#coffeeYesNo");
 let img = document.querySelector("img");
+
 window.onload = () =>{
 document.querySelector("#phaseOne").style.visibility = "visible";
 document.querySelector("#phaseTwo").style.visibility = "hidden";
@@ -37,32 +35,26 @@ button1.addEventListener("click", () => {
 displayName.innerText = "Hi, " + inputName.value;
 });
 
-//Display coffee or tea
 const thirdStep = () => {
     document.querySelector("#phaseOne").style.visibility = "hidden";
     document.querySelector("#phaseTwo").style.visibility = "hidden";
     document.querySelector("#phaseThree").style.visibility = "hidden";
     document.querySelector("#phaseFour").style.visibility = "visible";
-/*     document.querySelector("#coffee").style.visibility = "hidden";
-    document.querySelector("#tea").style.visibility = "hidden"; */
 };
-
-/* const yesNo = document.querySelector("#coffeeYesNo");
-yesNo.addEventListener("click", () =>{
-}); */
-
-const yesNo = document.querySelector("#coffeeYesNo");
 const displayImg = () => {
-    let img = document.querySelector("img");
     let value = yesNo.value;
-    if (value == "true"){
+    console.log(value);
+    
+    if (value === "true"){
         img.src = "img/coffee.jpg";
+    }
+    else{
+        img.src = "img/tea.jpg";
     }
 };
 
-displayImg();
-
 button2.addEventListener("click", thirdStep);
+button2.addEventListener("click", displayImg);
 
 const fourthStep = () => {
     document.querySelector("#phaseOne").style.visibility = "visible";
